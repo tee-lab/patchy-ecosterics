334df93e2cf704614b66bfc3af4856a579d96994
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2022-08-04 18:34:03 +0530

    Changed analysis scripts to read from new pkl data

--- plot_density.py
+++ plot_density.py
@@ -8,7 +8,10 @@ def plot_density(model_name, simulation_indices, measure_state = 1):
     """ Plots the time variation of density of a particular state """
     records = []
     for simulation_index in simulation_indices:
-        time_series = load_automaton_data(model_name, simulation_index)
+        automaton_data = load_automaton_data(model_name, simulation_index)
+        time_series = automaton_data["time_series"]
+        info = automaton_data["info"]
+
         num_frames = len(time_series)
         lattice_length = len(time_series[0])
         density_time_series = zeros(num_frames, dtype=float)
@@ -27,9 +30,12 @@ def plot_density(model_name, simulation_indices, measure_state = 1):
 
     print("Average density at final time-step:", averaged_densities[-1])
 
+    plt.title(info)
+    plt.xlabel("time")
+    plt.ylabel("density")
     plt.plot(averaged_densities)
     plt.show()  
 
 
 if __name__ == '__main__':
-    plot_density("tricritical", 1)
+    plot_density("contact_spatial", [0])
--- render_simulation.py
+++ render_simulation.py
@@ -1,10 +1,11 @@
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
-
+from numpy import sum, zeros
 from utils import load_automaton_data
 
 
 def animate(i):
+    plt.title(f"Frame {i}")
     im.set_array(time_series[i])
     return [im]
 
@@ -13,15 +14,28 @@ def render_simulation(model_name, simulation_index):
     """ Renders the simulation of a given automaton corresponding to the given model """
     global time_series, im
 
-    time_series = load_automaton_data(model_name, simulation_index)
+    automaton_data = load_automaton_data(model_name, simulation_index)
+    time_series, info = automaton_data["time_series"], automaton_data["info"]
     num_frames = len(time_series)
 
-    fig = plt.figure()
-    im = plt.imshow(time_series[0])
+    density_record = zeros(num_frames, dtype=float)
 
+    for i, lattice in enumerate(time_series):
+        num_state = sum(lattice == 1)
+        density_fraction = num_state / (len(lattice) * len(lattice))
+        density_record[i] = density_fraction
+
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.title(info)
+    plt.xlabel("time")
+    plt.ylabel("density")
+    plt.plot(density_record)
+    fig = plt.subplot(1, 2, 2).figure
+    im = plt.imshow(time_series[0])
     animation = FuncAnimation(fig, animate, frames=num_frames, interval=100, repeat=False)
     plt.show()
 
 
 if __name__ == '__main__':
-    render_simulation("contact_spatial", 0)
+    render_simulation("scanlon_kalahari", 2)
