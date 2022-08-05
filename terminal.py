746e5345feee9c4790bf25e40406e1f6325db4d8
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2022-08-05 18:11:47 +0530

    Generated phase diagram

--- terminal.py
+++ terminal.py
@@ -1,4 +1,5 @@
 # libraries
+from concurrent.futures import ThreadPoolExecutor
 from matplotlib import pyplot as plt
 from numpy import abs, arange, array, zeros
 # models
@@ -7,6 +8,7 @@ from models.scanlon_kalahari.main import scanlon_kalahari
 from models.tricritical.main import tricritical
 # analysis
 from cluster_dynamics import analyse_changes
+from cluster_equivalences import establish_equivalences
 from cluster_labelling import label_clusters
 from cluster_tracking import track_clusters
 from plot_density import plot_density
@@ -15,37 +17,38 @@ from render_simulation import render_simulation
 from utils import load_automaton_data
 
 
+
 if __name__ == '__main__':
     """ Write automated scripts here """
 
-    # phase transition
-    # p_range = arange(0, 1, 0.01)
-    # densities = zeros(len(p_range))
+    p_range = arange(0, 1, 0.01)
+    q_range = arange(0, 1, 0.01)
+    grid = zeros((len(p_range), len(q_range)))
 
-    # for i, p in enumerate(p_range):
-    #     print(f"p: {p}")
-    #     densities[i] = tricritical(p, 0,  save = False)
+    for i, q in enumerate(q_range):
+        for j, p in enumerate(p_range):
+            print(f"p: {p:.2f}, q: {q:.2f}")
+            grid[i, j] = tricritical(p, q, 24, False)
 
-    # plt.title("Bifurcation diagram of TDP for q = 0")
-    # plt.xlabel("p")
-    # plt.ylabel("mean density")
-    # plt.plot(p_range, densities)
-    # plt.show()
-    # plt.savefig("bifurcation_diagram.png")
-
-    # phase diagram
-    p_range = arange(0, 1, 0.1)
-    q_range = arange(0, 1, 0.1)
-    densities = zeros((len(p_range), len(q_range)))
-
-    for i, p in enumerate(p_range):
-        for j, q in enumerate(q_range):
-            print(f"p: {p}, q: {q}")
-            densities[j, i] = tricritical(p, q, 24, False)
-    
     plt.title("Phase diagram of TDP")
     plt.xlabel("p")
     plt.ylabel("q")
-    plt.imshow(densities, origin="lower", extent=[0, 1, 0, 1])
+    plt.imshow(grid, origin="lower", extent=[0, 1, 0, 1])
+    plt.colorbar()
     plt.show()
     plt.savefig("phase_diagram.png")
+
+    # automaton_data = load_automaton_data("tricritical", 0)
+    # time_series, info = automaton_data["time_series"], automaton_data["info"]
+    # lattice_1, lattice_2 = label_clusters(time_series[0]), label_clusters(time_series[1])
+
+    # track_clusters(lattice_1, lattice_2)
+    # establish_equivalences(lattice_1, lattice_2)
+
+    # plt.subplot(1, 2, 1)
+    # plt.title("Lattice 1")
+    # plt.imshow(lattice_1)
+    # plt.subplot(1, 2, 2)
+    # plt.title("Lattice 2")
+    # plt.imshow(lattice_2)
+    # plt.show()
