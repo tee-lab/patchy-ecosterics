7b16acce0311941936fb992a18349fdc0160f577
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2022-10-08 18:07:13 +0530

    Added print_cluster.py (prints cluster changes in every iteration)

---
+++ print_cluster.py
@@ -0,0 +1,14 @@
+from utils import load_automaton_data
+
+
+def print_cluster_data(model_name, simulation_index):
+    automaton_data = load_automaton_data(model_name, simulation_index, "cluster")
+    cluster_data, info = automaton_data["cluster_data"], automaton_data["info"]
+
+    print(info)
+    for iteration, update in enumerate(cluster_data):
+        print(f"iteration {iteration}: {update}")
+
+
+if __name__ == '__main__':
+    print_cluster_data("tricritical", 0)
