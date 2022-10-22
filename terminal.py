567330d3372839be4095080145644566813f5fbc
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2022-10-22 16:38:38 +0530

    Initialized repo for simulation on cluster 1

--- terminal.py
+++ terminal.py
@@ -14,14 +14,12 @@ from analyze_rates import analyze_rates
 from cluster_coefficient import cluster_coefficient
 from cluster_equivalences import establish_equivalences
 from cluster_tracking import track_clusters
-from plot_density import plot_density
 from purge_data import purge_data
-from render_simulation import render_simulation
 from utils import load_automaton_data
 
 
 if __name__ == '__main__':
     purge_data()
-    num_simulations = cpu_count() - 2
-    tricritical(0.32, 0.92, num_simulations, save_series=False, save_cluster=True)
-    analyze_rates("tricritical", range(num_simulations), plot_name="0p32")
+    num_simulations = cpu_count() - 8
+    tricritical(0.7, 0.0, num_simulations, save_series=False, save_cluster=True)
+    analyze_rates("tricritical", range(num_simulations), plot_name="0p70")
