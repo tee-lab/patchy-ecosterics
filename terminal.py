e6b85ee23ada4b72082586fb00be4db9f25041bc
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2022-12-23 00:00:50 +0530

    Init repo for sim on c2

--- terminal.py
+++ terminal.py
@@ -23,8 +23,8 @@ from utils import load_automaton_data
 
 
 if __name__ == '__main__':
-    num_simulations = cpu_count() - 1
-    p_values = [0.70, 0.71]
+    num_simulations = cpu_count() - 4
+    p_values = [0.72, 0.73, 0.74]
 
     for p in p_values:
         purge_data()
