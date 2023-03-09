685d3b0b2dca5a86453d77a9c44a238643da3be2
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2023-03-09 23:11:01 +0530

    Fixed memory leak

--- terminal.py
+++ terminal.py
@@ -34,6 +34,6 @@ if __name__ == '__main__':
         purge_data()
         print(f"\n---> Simulating p = {p} <---")
         file_string = str(p).replace('.', 'p')
-        tricritical(p, q, num_simulations, save_series=True, save_cluster=True)
+        tricritical(p, q, num_simulations, save_series=False, save_cluster=True)
         compile_changes("tricritical", range(num_simulations), plot_name=file_string)
         plot_changes(file_string)
