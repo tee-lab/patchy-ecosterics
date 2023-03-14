edbf9a0a4287eb5409fcc730e59d7edf7e02e8bf
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2023-03-15 04:09:15 +0530

    Completed fitting procedure

--- fitters/fit_cluster_sizes_2.R
+++ fitters/fit_cluster_sizes_2.R
@@ -23,7 +23,7 @@ q_folder = "q0"
 dataset = "100x100"
 
 q_value = "0"
-p_values = c("0p65", "0p7", "0p72", "0p74")
+p_values = c("0p63", "0p65", "0p7", "0p72", "0p74")
 
 root_path = file.path(results_path, model, q_folder, dataset)
 
