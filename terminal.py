051cafdfd6e99a4aa8599796860c5600e81e2f7e
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2023-03-16 05:33:28 +0530

    Init repo for confirmatory sim on 47

--- terminal.py
+++ terminal.py
@@ -28,8 +28,8 @@ from utils import load_automaton_data
 if __name__ == '__main__':
     set_start_method("spawn")
     num_simulations = cpu_count() - 1
-    p_values = [0.28, 0.285, 0.29, 0.31]
-    q = 0.92
+    p_values = [0.65, 0.7, 0.72, 0.74]
+    q = 0
 
     for p in p_values:
         purge_data()
