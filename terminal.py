685f1c87647e0dd0e144af6c8013e83c9eda088f
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2023-03-30 10:39:45 +0530

    Init repo for sim on 211

--- terminal.py
+++ terminal.py
@@ -28,8 +28,8 @@ from utils import load_automaton_data
 if __name__ == '__main__':
     set_start_method("spawn")
     num_simulations = cpu_count() - 1
-    p_values = [0.6, 0.62, 0.65, 0.67]
-    q = 0.25
+    p_values = [0.405, 0.41, 0.42, 0.44]
+    q = 0.75
 
     for p in p_values:
         purge_data()
