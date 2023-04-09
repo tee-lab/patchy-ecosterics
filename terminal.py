f943ecb41060334453b7ae8a4a62c060a3d9d0e9
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2023-04-09 19:39:23 +0530

    Init repo for sim on 47

--- terminal.py
+++ terminal.py
@@ -29,13 +29,12 @@ from utils import load_automaton_data
 if __name__ == '__main__':
     set_start_method("spawn")
     num_simulations = cpu_count() - 1
-    p_values = [0.282, 0.283, 0.285, 0.29]
-    q = 0.92
+    f_values = [0.27, 0.48, 0.54, 0.61]
 
-    for p in p_values:
+    for f in f_values:
         purge_data()
-        print(f"\n---> Simulating p = {p} <---")
-        file_string = str(p).replace('.', 'p')
-        tricritical(p, q, num_simulations, save_series=False, save_cluster=True)
-        compile_changes("tricritical", range(num_simulations), plot_name=file_string)
+        print(f"\n---> Simulating f = {f} <---")
+        file_string = str(f).replace('.', 'p')
+        null_stochastic(f, num_simulations, save_series=False, save_cluster=True)
+        compile_changes("null_stochastic", range(num_simulations), plot_name=file_string)
         plot_changes(file_string)
