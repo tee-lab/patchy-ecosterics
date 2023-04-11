cf7083cfe9c59143abf0b77bf178c09cfb30cb24
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2023-04-11 16:20:07 +0530

    Stray commits

---
+++ plotters/null.py
@@ -0,0 +1,163 @@
+from matplotlib import pyplot as plt
+from numpy import delete, loadtxt, transpose, zeros
+from os import path
+from tqdm import tqdm
+
+
+def get_cluster_distribution(data_path, folder, file_name):
+    file_path = path.join(data_path, folder, file_name)
+    cluster_distribution_data = transpose(loadtxt(open(file_path, 'r')))
+    cluster_sizes, num = cluster_distribution_data[0][1:], cluster_distribution_data[1][1:]
+
+    inverse_cdf = zeros(len(num))
+    for i in range(len(num)):
+        inverse_cdf[i] = sum(num[i:])
+    inverse_cdf = inverse_cdf / sum(num)
+
+    remove_indices = []
+    for i in range(len(cluster_sizes) - 1):
+        if inverse_cdf[i] == inverse_cdf[i + 1]:
+            remove_indices.append(i)
+
+    cluster_sizes = delete(cluster_sizes, remove_indices)
+    inverse_cdf = delete(inverse_cdf, remove_indices)
+
+    return cluster_sizes, inverse_cdf
+
+
+def get_cluster_dynamics(data_path, folder, file_name):
+    file_path = path.join(data_path, folder, file_name)
+    changes_data = transpose(loadtxt(open(file_path, 'r')))
+    changes, changes_histogram = list(changes_data[0]), changes_data[1]
+    changes_probabilities = changes_histogram / sum(changes_histogram)
+
+    abs_changes = list(range(0, int(max(max(changes), -min(changes)))))
+    abs_changes_histogram = [0] * len(abs_changes)
+
+    for abs_change in abs_changes:
+        value = 0
+
+        if abs_change in changes:
+            value += changes_probabilities[changes.index(abs_change)]
+        if -abs_change in changes:
+            value += changes_probabilities[changes.index(-abs_change)]
+        
+        abs_changes_histogram[abs_change] = value
+
+    abs_changes_histogram[0] = abs_changes_histogram[0] / 2
+
+    probability_distribution = abs_changes_histogram / sum(abs_changes_histogram)
+    inverse_cdf = zeros(len(probability_distribution))
+    for i in range(len(probability_distribution)):
+        inverse_cdf[i] = sum(probability_distribution[i:])
+    inverse_cdf = inverse_cdf / sum(inverse_cdf)
+
+    return abs_changes[3:], inverse_cdf[3:]
+
+
+def get_mean_ds(data_path, name, limit):
+    folder = path.join(data_path, name)
+    cluster_ds_data = transpose(loadtxt(open(path.join(folder, name + '_cluster_ds.txt'), 'r')))
+    cluster_analyze_limit = min(limit, len(cluster_ds_data[0]))
+    cluster_ds_data = cluster_ds_data[1]
+
+    return range(cluster_analyze_limit), cluster_ds_data[:cluster_analyze_limit]
+
+
+def get_mean_ds_sq(data_path, name, limit):
+    folder = path.join(data_path, name)
+    cluster_ds_data = transpose(loadtxt(open(path.join(folder, name + '_cluster_ds.txt'), 'r')))
+    cluster_analyze_limit = min(limit, len(cluster_ds_data[0]))
+    cluster_ds_data = cluster_ds_data[2]
+
+    return range(cluster_analyze_limit), cluster_ds_data[:cluster_analyze_limit]
+
+
+if __name__ == '__main__':
+    root_path = path.join(path.dirname(__file__), "..")
+
+    q = 0
+    p_values = [0.65, 0.7, 0.72, 0.74]
+    densities = [0.27, 0.48, 0.54, 0.61]
+    tdp_cluster_limits = [100, 500, 4000, 6000]
+    null_cluster_limits = [50, 250, 2000, 6000]
+    dataset = "100x100"
+
+    assert len(p_values) == len(densities) == len(tdp_cluster_limits)
+
+    q_folder = "q" + str(q).replace(".", "p") 
+    null_path = path.join("results", "null_stochastic")
+    tdp_path = path.join("results", "tricritical", q_folder, dataset)
+
+    plt.subplots(4, len(p_values), figsize=(40, 25))
+    for i in tqdm(range(len(p_values))):
+        col = i + 1
+        p = p_values[i]
+        density = densities[i]
+
+        tdp_folder = str(p).replace(".", "p")
+        null_folder = str(density).replace(".", "p")
+
+        row = 1
+        tdp_file_name = tdp_folder + "_cluster_distribution.txt"
+        tdp_cluster_sizes, tdp_inverse_cdf = get_cluster_distribution(tdp_path, tdp_folder, tdp_file_name)
+        null_file_name = null_folder + "_cluster_distribution.txt"
+        null_cluster_sizes, null_inverse_cdf = get_cluster_distribution(null_path, null_folder, null_file_name)
+
+        plt.subplot(4, len(p_values), col)
+        plt.title("Cluster Size Distribution")
+        plt.xlabel("s")
+        if col == 1:
+            plt.ylabel("P(S > s)")
+        plt.loglog(tdp_cluster_sizes, tdp_inverse_cdf, 'o', label="TDP")
+        plt.loglog(null_cluster_sizes, null_inverse_cdf, 'o', label="Null")
+        plt.legend()
+
+        row = 2
+        tdp_file_name = tdp_folder + "_changes.txt"
+        tdp_changes, tdp_inverse_cdf = get_cluster_dynamics(tdp_path, tdp_folder, tdp_file_name)
+        null_file_name = null_folder + "_changes.txt"
+        null_changes, null_inverse_cdf = get_cluster_dynamics(null_path, null_folder, null_file_name)
+
+        plt.subplot(4, len(p_values), col + len(p_values))
+        plt.title("Cluster Dynamics")
+        plt.xlabel("ds")
+        if col == 1:
+            plt.ylabel("P(dS > ds)")
+        plt.loglog(tdp_changes, tdp_inverse_cdf, label="TDP")
+        plt.loglog(null_changes, null_inverse_cdf, label="Null")
+        plt.legend()
+
+        row = 3
+        tdp_file_name = tdp_folder + "_cluster_ds.txt"
+        tdp_cluster_sizes, tdp_cluster_ds = get_mean_ds(tdp_path, tdp_folder, tdp_cluster_limits[i])
+        null_file_name = null_folder + "_cluster_ds.txt"
+        null_cluster_sizes, null_cluster_ds = get_mean_ds(null_path, null_folder, null_cluster_limits[i])
+
+        plt.subplot(4, len(p_values), col + 2 * len(p_values))
+        plt.title("Drift Term")
+        plt.xlabel("s")
+        if col == 1:
+            plt.ylabel("Mean ds")
+        plt.plot(tdp_cluster_sizes, tdp_cluster_ds, label="TDP")
+        plt.plot(null_cluster_sizes, null_cluster_ds, label="Null")
+        plt.plot([0, max(tdp_cluster_limits[i], null_cluster_limits[i])], [0, 0], 'k--')
+        plt.legend()
+
+        row = 4
+        tdp_file_name = tdp_folder + "_cluster_ds.txt"
+        tdp_cluster_sizes, tdp_cluster_ds_sq = get_mean_ds_sq(tdp_path, tdp_folder, tdp_cluster_limits[i])
+        null_file_name = null_folder + "_cluster_ds.txt"
+        null_cluster_sizes, null_cluster_ds_sq = get_mean_ds_sq(null_path, null_folder, null_cluster_limits[i])
+
+        plt.subplot(4, len(p_values), col + 3 * len(p_values))
+        plt.title("Diffusion Term")
+        plt.xlabel("s")
+        if col == 1:
+            plt.ylabel("Mean (ds^2)")
+        plt.plot(tdp_cluster_sizes, tdp_cluster_ds_sq, label="TDP")
+        plt.plot(null_cluster_sizes, null_cluster_ds_sq, label="Null")
+        plt.legend()
+
+    plt.savefig(f"q{q}_null.png", bbox_inches='tight')
+    plt.show()
--- plotters/sde.py
+++ plotters/sde.py
@@ -26,38 +26,91 @@ if __name__ == '__main__':
     ####################################
     # Tricritical Directed Percolation #
     ####################################
-    # results_path = path.join(path.dirname(path.dirname(__file__)), 'results')
-    # model = "tricritical"
-    # dataset = "100x100_2"
+    results_path = path.join(path.dirname(path.dirname(__file__)), 'results')
+    model = "tricritical"
+    dataset = "100x100"
 
     # q = 0
     # p_values = [0.65, 0.7, 0.72, 0.74]
     # cluster_limits = [100, 500, 4000, 6000]
 
-    # # q = 0.5
-    # # p_values = [0.5, 0.53, 0.55, 0.57]
-    # # cluster_limits = [100, 1000, 4000, 5000]
+    # q = 0.25
+    # p_values = [0.6, 0.62, 0.65, 0.67]
+    # cluster_limits = [100, 500, 4000, 6000]
 
-    # # q = 0.92
-    # # p_values = [0.28, 0.285, 0.29, 0.31]
-    # # cluster_limits = [1000, 1000, 1000, 1000]
+    # q = 0.5
+    # p_values = [0.5, 0.53, 0.55, 0.57]
+    # cluster_limits = [100, 500, 4000, 6000]
 
-    # subfolder = "q" + str(q).replace('.', 'p')
-    # data_path = path.join(results_path, model, subfolder, dataset)
+    # q = 0.75
+    # p_values = [0.405, 0.41, 0.42, 0.44]
+    # cluster_limits = [200, 1000, 4000, 6000]
 
-    # num_cols = len(p_values)
+    q = 0.92
+    p_values = [0.282, 0.283, 0.285, 0.29]
+    cluster_limits = [300, 600, 1000, 1000]
+
+    subfolder = "q" + str(q).replace('.', 'p')
+    data_path = path.join(results_path, model, subfolder, dataset)
+
+    num_cols = len(p_values)
+    plt.subplots(2, num_cols, figsize=(20, 10))
+
+    for i in tqdm(range(num_cols)):
+        col = i + 1
+        p = p_values[i]
+        limit = cluster_limits[i]
+        name = str(p).replace('.', 'p')
+
+        row = 0
+        cluster_sizes, mean_ds = get_mean_ds(name, limit)
+        plt.subplot(2, num_cols, row * num_cols + col)
+        plt.title(f"p = {p}")
+
+        if row == 2:
+            plt.xlabel("Cluster Size")
+
+        if col == 1:
+            plt.ylabel("Mean dS")
+        plt.plot(cluster_sizes[1:], mean_ds[1:])
+        plt.plot([0, limit], [0, 0], 'k--')
+
+        row = 1
+        cluster_sizes, mean_ds_sq = get_mean_ds_sq(name, limit)
+        plt.subplot(2, num_cols, row * num_cols + col)
+        plt.xlabel("Cluster Size")
+
+        if col == 1:
+            plt.ylabel("Mean (dS^2)")
+        plt.plot(cluster_sizes, mean_ds_sq)
+    
+    plt.savefig(subfolder + "_cluster_sde.png", bbox_inches='tight')
+    plt.show()
+
+    ####################
+    # Scanlon Kalahari #
+    ####################
+    # results_path = path.join(path.dirname(path.dirname(__file__)), 'results')
+    # model = "scanlon_kalahari"
+    # dataset = "100x100"
+    # rainfall_values = [300, 500, 700, 900]
+    # cluster_limits = [100, 500, 1000, 2000]
+
+    # data_path = path.join(results_path, model, dataset)
+
+    # num_cols = len(rainfall_values)
     # plt.subplots(2, num_cols, figsize=(20, 10))
 
     # for i in tqdm(range(num_cols)):
     #     col = i + 1
-    #     p = p_values[i]
+    #     rainfall = rainfall_values[i]
     #     limit = cluster_limits[i]
-    #     name = str(p).replace('.', 'p')
+    #     name = str(rainfall)
 
     #     row = 0
     #     cluster_sizes, mean_ds = get_mean_ds(name, limit)
     #     plt.subplot(2, num_cols, row * num_cols + col)
-    #     plt.title(f"p = {p}")
+    #     plt.title(f"Rainfall = {rainfall}")
     #     plt.xlabel("Cluster Size")
 
     #     if col == 1:
@@ -74,48 +127,5 @@ if __name__ == '__main__':
     #         plt.ylabel("Mean (dS^2)")
     #     plt.plot(cluster_sizes, mean_ds_sq)
     
-    # plt.savefig(subfolder + "_cluster_sde.png")
+    # plt.savefig("scanlon_cluster_sde.png")
     # plt.show()
-
-    ####################
-    # Scanlon Kalahari #
-    ####################
-    results_path = path.join(path.dirname(path.dirname(__file__)), 'results')
-    model = "scanlon_kalahari"
-    dataset = "100x100"
-    rainfall_values = [300, 500, 700, 900]
-    cluster_limits = [100, 500, 1000, 2000]
-
-    data_path = path.join(results_path, model, dataset)
-
-    num_cols = len(rainfall_values)
-    plt.subplots(2, num_cols, figsize=(20, 10))
-
-    for i in tqdm(range(num_cols)):
-        col = i + 1
-        rainfall = rainfall_values[i]
-        limit = cluster_limits[i]
-        name = str(rainfall)
-
-        row = 0
-        cluster_sizes, mean_ds = get_mean_ds(name, limit)
-        plt.subplot(2, num_cols, row * num_cols + col)
-        plt.title(f"Rainfall = {rainfall}")
-        plt.xlabel("Cluster Size")
-
-        if col == 1:
-            plt.ylabel("Mean dS")
-        plt.plot(cluster_sizes, mean_ds)
-        plt.plot([0, limit], [0, 0], 'k--')
-
-        row = 1
-        cluster_sizes, mean_ds_sq = get_mean_ds_sq(name, limit)
-        plt.subplot(2, num_cols, row * num_cols + col)
-        plt.xlabel("Cluster Size")
-
-        if col == 1:
-            plt.ylabel("Mean (dS^2)")
-        plt.plot(cluster_sizes, mean_ds_sq)
-    
-    plt.savefig("scanlon_cluster_sde.png")
-    plt.show()
