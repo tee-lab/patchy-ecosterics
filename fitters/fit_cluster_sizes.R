88a54df019e0f2de591d6f42474f6129f33eeb79
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2023-09-14 11:20:35 +0530

    Compiled results

--- fitters/fit_cluster_sizes.R
+++ fitters/fit_cluster_sizes.R
@@ -3,13 +3,26 @@ library(spatialwarnings)
 
 results_path = "C://Code//Github//vegetation-dynamics//results"
 model = "tricritical"
-q_folder = "q0"
 dataset = "100x100_final"
 
-q_value = "0"
-p_values = c("0p616", "0p618", "0p62", "0p625", "0p63", "0p64", "0p65", "0p7", "0p72")
+# q_folder = "q0"
+# q_value = "0"
+# p_values = c("0p616", "0p618", "0p62", "0p625", "0p63", "0p64", "0p65", "0p7", "0p72")
+
+# q_folder = "q0p25"
+# q_value = "0p25"
+# p_values = c("0p566", "0p569", "0p57", "0p575", "0p58", "0p59", "0p62", "0p64")
+
+# q_folder = "q0p5"
+# q_value = "0p5"
+# p_values = c("0p498", "0p5", "0p502", "0p504", "0p506", "0p508", "0p51", "0p52", "0p53", "0p55")
+
+q_folder = "q0p75"
+q_value = "0p75"
+p_values = c("0p399", "0p4", "0p401", "0p403", "0p405", "0p41", "0p42")
 
 root_path = file.path(results_path, model, q_folder, dataset)
+data_frame = data.frame()
 
 for (p in p_values) {
   print(paste("<--- Analyzing", p, "--->"))
@@ -49,4 +62,13 @@ for (p in p_values) {
   print(pl_bic_mean)
   print(tpl_bic_mean)
   print(exp_bic_mean)
+  
+  data_frame = rbind(data_frame, c(p, pl_bic_mean, tpl_bic_mean, exp_bic_mean))
 }
+
+colnames(data_frame)[1] = "p"
+colnames(data_frame)[2] = "PL"
+colnames(data_frame)[3] = "TPL"
+colnames(data_frame)[4] = "Exp"
+
+write.csv(data_frame, paste(q_folder, "_csd", ".csv", sep=""))
