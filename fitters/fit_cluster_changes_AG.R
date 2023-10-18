3a9b749790d89fa457d43511994c5721e3e2a3b5
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2023-10-18 13:00:43 +0530

    Compiled results

--- fitters/fit_cluster_changes_AG.R
+++ fitters/fit_cluster_changes_AG.R
@@ -102,15 +102,15 @@ fit_tpl = TRUE
 
 # options(spatialwarnings.constants.reltol = 1e-4)
 
-q_folder = "q0"
+# q_folder = "q0"
 # p_values = c("test") # ideal for debugging
-p_values = c("0p616", "0p618", "0p62", "0p625", "0p63", "0p64", "0p65", "0p7", "0p72")
+# p_values = c("0p616", "0p618", "0p62", "0p625", "0p63", "0p64", "0p65", "0p7", "0p72")
 
 # q_folder = "q0p25"
 # p_values = c("0p566", "0p568", "0p57", "0p575", "0p58", "0p59", "0p62", "0p64")
 
-# q_folder = "q0p5"
-# p_values = c("0p498", "0p5", "0p502", "0p504", "0p506", "0p508", "0p51", "0p52", "0p53", "0p55")
+q_folder = "q0p5"
+p_values = c("0p498", "0p5", "0p502", "0p504", "0p506", "0p508", "0p51", "0p52", "0p53", "0p55")
 
 # q_folder = "q0p75"
 # p_values = c("0p399", "0p4", "0p401", "0p403", "0p405", "0p41", "0p42")
