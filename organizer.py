60c3ed2497f3be6303a42027e492a400790b434d
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2024-01-21 19:51:14 +0530

    Compiled results

--- organizer.py
+++ organizer.py
@@ -12,15 +12,17 @@ from shutil import move
 if __name__ == '__main__':
     output_path = path.join(path.dirname(__file__), "outputs")
 
-    p_values = [0.54, 0.57]
+    p_values = [0.5]
     # p_values = [0.616, 0.618, 0.62, 0.625, 0.63, 0.64, 0.65, 0.7, 0.72]
     # p_values = [0.566, 0.569, 0.57, 0.575, 0.58, 0.59, 0.62, 0.64]
     # p_values = [0.498, 0.5, 0.502, 0.504, 0.506, 0.508, 0.51, 0.52, 0.53, 0.55]
     # p_values = [0.399, 0.4, 0.401, 0.403, 0.405, 0.41, 0.42]
     # p_values = [300, 400, 500, 600, 700, 770, 800, 830, 900]
 
-    params = p_values
-    prefixes = [str(p).replace('.', 'p') for p in params]
+    # params = p_values
+    # prefixes = [str(p).replace('.', 'p') for p in params]
+
+    prefixes = ["q0"]
 
     for prefix in prefixes:
         makedirs(path.join(output_path, prefix), exist_ok=True)
