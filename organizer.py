b9b4eeeaa69c8ec102f5d8ef53a95837b15a04fc
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2024-06-10 21:32:09 +0530

    Compiled results

--- organizer.py
+++ organizer.py
@@ -12,14 +12,14 @@ from shutil import move
 if __name__ == '__main__':
     output_path = path.join(path.dirname(__file__), "outputs")
 
-    p_values = [0.65]
+    # p_values = [0.7]
     # p_values = [0.282, 0.283, 0.284, 0.285, 0.29]
     # p_values = [0.616, 0.618, 0.62, 0.625, 0.63, 0.64, 0.65, 0.7, 0.72]
     # p_values = [0.566, 0.569, 0.57, 0.575, 0.58, 0.59, 0.62, 0.64]
     # p_values = [0.498, 0.5, 0.502, 0.504, 0.506, 0.508, 0.51, 0.52, 0.53, 0.55]
     # p_values = [0.399, 0.4, 0.401, 0.403, 0.405, 0.41, 0.42]
     # p_values = [300, 400, 500, 600, 700, 770, 800, 830, 900]
-    # p_values = [500, 770, 850]
+    p_values = [500, 770, 850]
 
     params = p_values
     prefixes = [str(p).replace('.', 'p') for p in params]
